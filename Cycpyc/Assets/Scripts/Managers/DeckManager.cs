613b9a17b03d18cecbef9e9539f4d62ac5f0337f
GlareAtMe <ronpo.amx.st@gmail.com>
2025-05-16 01:25:39 +0300

    add base card manager + add test and base deck + test deck

---
+++ Cycpyc/Assets/Scripts/Managers/DeckManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DeckManager : MonoBehaviour
+{
+    [SerializeField] private DeckDataSO defaultDeckData;
+    private List<CardDataSO> deck;
+
+    private void Awake()
+    {
+        LoadDeck();
+    }
+
+    public void LoadDeck()
+    {
+        LoadDeck(defaultDeckData);
+    }
+
+    public void LoadDeck(DeckDataSO deckDataToLoad)
+    {
+        deck = new List<CardDataSO>(deckDataToLoad.cardsDeck);
+        Debug.Log($"Deck loaded with {deck.Count} cards.");
+    }
+
+    public CardDataSO DrawCard()
+    {
+        if (deck.Count == 0)
+        {
+            Debug.LogWarning("Deck is empty!");
+            return null;
+        }
+
+        CardDataSO drawnCard = deck[0];
+        deck.RemoveAt(0);
+        Debug.Log($"Drew card: {drawnCard.name}");
+        return drawnCard;
+    }
+
+    public void ShuffleDeck()
+    {
+        for (int i = 0; i < deck.Count; i++)
+        {
+            CardDataSO temp = deck[i];
+            int randomIndex = Random.Range(i, deck.Count);
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
+
+        Debug.Log("Deck shuffled.");
+    }
+
+    public List<CardDataSO> GetDeck()
+    {
+        return deck;
+    }
+}
---
+++ Cycpyc/Assets/Scripts/ScriptableObjects/CardDeckS/CardDeckSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "Cards/DeckData")]
+public class DeckDataSO : ScriptableObject
+{
+    [Tooltip("List of cards in this deck.")]
+    public List<CardDataSO> cardsDeck;
+}
